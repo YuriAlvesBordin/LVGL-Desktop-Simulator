96e6f0009fcc02b1d2d3bd3c87f8820c1334b1c6
yurialvesbordin <yuribordin@hotmail.com>
2026-08-27 19:57:37 -0300

    Add ./src/app/Application.h: #ifndef LVGL_GLFW_APP_APPLICATION_H

---
+++ src/app/Application.h
@@ -0,0 +1,14 @@
+#ifndef LVGL_GLFW_APP_APPLICATION_H
+#define LVGL_GLFW_APP_APPLICATION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void app_application_initialize(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

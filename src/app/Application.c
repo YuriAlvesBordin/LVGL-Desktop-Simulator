508e106b412667560bedbdae43f2882eec5ecd23
yurialvesbordin <yuribordin@hotmail.com>
2026-08-27 22:22:32 -0300

    Modify application initialization to show a button with label instead of default widget demo

--- src/app/Application.c
+++ src/app/Application.c
@@ -5,5 +5,13 @@
 
 void app_application_initialize(void)
 {
-    lv_demo_widgets();
+    // lv_demo_widgets();
+
+    lv_obj_t *btn = lv_button_create(lv_screen_active());
+    lv_obj_center(btn);
+
+    lv_obj_t *label = lv_label_create(btn);
+    lv_label_set_text(label, "Live preview");
+    lv_obj_center(label);
+
 }

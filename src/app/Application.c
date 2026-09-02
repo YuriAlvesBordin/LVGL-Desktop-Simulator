7738fe465211c7b88076fd9ddfb6c7be4dbd91ec
Yuri Alves Bordin <yuri.bordin@kurokawacontrols.com>
2026-09-02 12:13:50 -0300

    feat: replace demo widgets with screen handler initialization

--- src/app/Application.c
+++ src/app/Application.c
@@ -1,17 +1,9 @@
 #include "Application.h"
 
 #include <lvgl.h>
-#include <widgets/lv_demo_widgets.h>
+#include <screenHandler.h>
 
 void app_application_initialize(void)
 {
-    // lv_demo_widgets();
-
-    lv_obj_t *btn = lv_button_create(lv_screen_active());
-    lv_obj_center(btn);
-
-    lv_obj_t *label = lv_label_create(btn);
-    lv_label_set_text(label, "Live preview");
-    lv_obj_center(label);
-
+    screen_handler_init();
 }
